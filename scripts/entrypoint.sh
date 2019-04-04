363afb64be05786ed040b49b554aa5bd139639e0
iromli <isman.firmansyah@gmail.com>
2019-04-05 02:36:58 +0700

    show license info

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+cat << LICENSE_ACK
+
+# ========================================================================================= #
+# Gluu License Agreement: https://github.com/GluuFederation/gluu-docker/blob/3.1.5/LICENSE. #
+# The use of Gluu Server Docker Edition is subject to the Gluu Support License.             #
+# ========================================================================================= #
+
+LICENSE_ACK
+
 if [ -f /etc/redhat-release ]; then
     source scl_source enable python27 && python /opt/scripts/entrypoint.py
 else
